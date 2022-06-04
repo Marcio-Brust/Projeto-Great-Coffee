46b9218a86e4c1f960c6fa201fc1d3f39ed8c91e
Marcio-Brust <brustmw21@gmail.com>
2022-06-04 17:36:50 -0300

    Projeto Great Coffee

---
+++ Great Coffee/script.js
@@ -0,0 +1,20 @@
+const $ = document.querySelector.bind(document);
+const btnMobile = $(".btn-mobile");
+const footer = $(".rodape");
+const width = window.innerWidth;
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = $(".nav");
+  nav.classList.toggle("active");
+  footer.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    btnMobile.innerHTML = '<img src="assets/img/menu-buguer-close.svg" alt>';
+  } else {
+    btnMobile.innerHTML = '<img src="assets/img/menu-buguer-open.svg" alt>';
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
