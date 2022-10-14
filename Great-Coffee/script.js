9f39ce6a9c52bb3ce0b5974a6a5f5b4310c91d36
Márcio Brust <brustmw21@gmail.com>
2022-10-14 08:51:19 -0300

    atualizacao no projeto

--- Great-Coffee/script.js
+++ Great-Coffee/script.js
@@ -1,21 +1,20 @@
 const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 const btnMobile = $(".btn-mobile");
 const footer = $(".rodape");
-const eventos = ["click", "touchstart"];
+const link = $$(".menu a");
 
 function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
   const nav = $(".nav");
   nav.classList.toggle("active");
   footer.classList.toggle("active");
   const active = nav.classList.contains("active");
   event.currentTarget.setAttribute("aria-expanded", active);
-  if (active) {
+  if (active)
     btnMobile.innerHTML = '<img src="assets/img/menu-buguer-close.svg" alt>';
-  } else {
-    btnMobile.innerHTML = '<img src="assets/img/menu-buguer-open.svg" alt>';
-  }
+  else btnMobile.innerHTML = '<img src="assets/img/menu-buguer-open.svg" alt>';
 }
 
-eventos.forEach((eventos) => {
-  btnMobile.addEventListener(eventos, toggleMenu);
-});
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
